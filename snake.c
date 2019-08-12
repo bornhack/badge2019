1f5e81ad7854c1c9d0144cf085105bd787893247
Niels Kristensen <niels@kristensen.io>
2019-08-12 14:12:15 +0200

    fix indentation of b0rken editor

--- snake.c
+++ snake.c
@@ -27,164 +27,164 @@
 #include <stdlib.h>
 
 enum events {
-	EV_UP = 1,
-	EV_DOWN,
-	EV_LEFT,
-	EV_RIGHT,
+  EV_UP = 1,
+  EV_DOWN,
+  EV_LEFT,
+  EV_RIGHT,
 };
 
 static const struct button_config snake_buttons[BTN_MAX] = {
-	[BTN_UP]     = { .press   = EV_UP, },
-	[BTN_DOWN]   = { .press   = EV_DOWN, },
-	[BTN_LEFT]   = { .press   = EV_LEFT, },
-	[BTN_RIGHT]  = { .press   = EV_RIGHT, },
+  [BTN_UP]     = { .press   = EV_UP, },
+  [BTN_DOWN]   = { .press   = EV_DOWN, },
+  [BTN_LEFT]   = { .press   = EV_LEFT, },
+  [BTN_RIGHT]  = { .press   = EV_RIGHT, },
 };
 
 struct pos {
-	char x,
-	     y;
+  char x,
+       y;
 };
 
 static struct pos getRandomPos(){
-	short x, y;
-	struct pos point;
+  short x, y;
+  struct pos point;
 
-	x = rand() % 44 * 5 + 10;
-	y = rand() % 44 * 5 + 10;
+  x = rand() % 44 * 5 + 10;
+  y = rand() % 44 * 5 + 10;
 
-	point.x = x;
-	point.y = y;
-	return point;
+  point.x = x;
+  point.y = y;
+  return point;
 }
 
 static void lost(unsigned short len){
   short xpos, printchar;
 
-	dp_fill(0, 0, 240, 240, 0x000);
-	dp_puts(30, 20, 0x00F, 0x00, "Your score:");
-	xpos = 220;
+  dp_fill(0, 0, 240, 240, 0x000);
+  dp_puts(30, 20, 0x00F, 0x00, "Your score:");
+  xpos = 220;
 
   if (xpos == 220 && len == 0) {
     dp_putchar(xpos, 50, 0xCB0, 0x000, 48);
-	} else {	
-		while (len != 0) {
-			printchar = len % 10 + 48;
-			dp_putchar(xpos, 50, 0xCB0, 0x000, printchar);
-			len /= 10;
-			xpos -= 15;
-		}
-	}
-	timer_msleep(5000);
+  } else {	
+    while (len != 0) {
+      printchar = len % 10 + 48;
+      dp_putchar(xpos, 50, 0xCB0, 0x000, printchar);
+      len /= 10;
+      xpos -= 15;
+    }
+  }
+  timer_msleep(5000);
 }
 
 void
 snake(void)
 {
   struct pos point;
-	struct pos path[256];
+  struct pos path[256];
 
   unsigned short i = 0, k = 0, last = 0, skip = 0, len = 5;
 
-	// clear and print border
+  // clear and print border
   dp_fill(0, 0, 240, 240, 0x000);
-	dp_fill(5, 5, 5, 235, 0xCB0);
-	dp_fill(235, 5, 5, 235, 0xCB0);
-	dp_fill(10, 5, 230, 5, 0xCB0);
-	dp_fill(10, 235, 230, 5, 0xCB0);
+  dp_fill(5, 5, 5, 235, 0xCB0);
+  dp_fill(235, 5, 5, 235, 0xCB0);
+  dp_fill(10, 5, 230, 5, 0xCB0);
+  dp_fill(10, 235, 230, 5, 0xCB0);
 
-	buttons_config(snake_buttons);
+  buttons_config(snake_buttons);
 
   // start direction
   char direction = 'd';
 
-	// start position
-	unsigned short currentX = 30,
-	      	       currentY = 30;
+  // start position
+  unsigned short currentX = 30,
+           currentY = 30;
 
   // first point
-	point.x = 200;
-	point.y = 200;
+  point.x = 200;
+  point.y = 200;
   dp_fill(point.x, point.y, 5, 5, 0xF00);
 
-	// '4' Guaranteed to be randomly -- chosen by fair dice
+  // '4' Guaranteed to be randomly -- chosen by fair dice
   srand(4);
 
   i = 0;
-	while (1) {
+  while (1) {
 
-    switch ((enum events)event_get()) {
-		case EV_UP:
-			if (direction != 'd')
-			  direction = 'u';
-			break;
-		case EV_DOWN:
-			if (direction != 'u')
-			  direction = 'd';
-			break;
-		case EV_LEFT:
-			if (direction != 'r')
-			  direction = 'l';
-			break;
-		case EV_RIGHT:
-			if (direction != 'l')
-			  direction = 'r';
-			break;
-		}
+  switch ((enum events)event_get()) {
+    case EV_UP:
+      if (direction != 'd')
+        direction = 'u';
+      break;
+    case EV_DOWN:
+      if (direction != 'u')
+        direction = 'd';
+      break;
+    case EV_LEFT:
+      if (direction != 'r')
+        direction = 'l';
+      break;
+    case EV_RIGHT:
+      if (direction != 'l')
+        direction = 'r';
+      break;
+    }
 
-		if (direction == 'd'){
-			currentY += 5;
-		} else if (direction == 'u'){
-			currentY -= 5;
-		} else if (direction == 'l'){
-			currentX -= 5;
-		} else if (direction == 'r'){
-			currentX += 5;
-		}
+    if (direction == 'd'){
+      currentY += 5;
+    } else if (direction == 'u'){
+      currentY -= 5;
+    } else if (direction == 'l'){
+      currentX -= 5;
+    } else if (direction == 'r'){
+      currentX += 5;
+    }
 
     // Did we hit ourself?
     unsigned int a, b, c;
-		if (i > 1) {
-			b = i - 2;
-			c = i - len - 1;
-			for(a = c; a <= b; a++)
-			{
-				if(currentX == path[a].x && currentY == path[a].y){
-					lost(len - 5);
-					return;
-				}
-			}
-		}
+    if (i > 1) {
+      b = i - 2;
+      c = i - len - 1;
+      for(a = c; a <= b; a++)
+      {
+        if(currentX == path[a].x && currentY == path[a].y){
+          lost(len - 5);
+          return;
+        }
+      }
+    }
 
     dp_fill(currentX, currentY, 5, 5, 0xCB0);
 
-		path[i].x = currentX;
-		path[i].y = currentY;
+    path[i].x = currentX;
+    path[i].y = currentY;
     i = (i + 1) % ARRAY_SIZE(path);
 
     if (k > 5 && !skip) {
       dp_fill(path[last].x, path[last].y, 5, 5, 0x000);
-			last = (last + 1) % ARRAY_SIZE(path);
-		} else {
-			skip = 0;
-			k++;
-		}
+      last = (last + 1) % ARRAY_SIZE(path);
+    } else {
+      skip = 0;
+      k++;
+    }
 
-		// Did we eat a point?
-		if (currentX == point.x && currentY == point.y){
-			skip = 1;
-			len++;
-			// generate new point
+    // Did we eat a point?
+    if (currentX == point.x && currentY == point.y){
+      skip = 1;
+      len++;
+      // generate new point
       point = getRandomPos();
-		}
+    }
 
-		dp_fill(point.x, point.y, 5, 5, 0xF00);
+    dp_fill(point.x, point.y, 5, 5, 0xF00);
 
-		// Did we hit the wall?
-		if (currentY == 235 || currentY == 5 || currentX == 5 || currentX == 235){
+    // Did we hit the wall?
+    if (currentY == 235 || currentY == 5 || currentX == 5 || currentX == 235){
       lost(len - 5);
-			return;
-		}
+      return;
+    }
 
-		timer_msleep(100);
-	}
+    timer_msleep(100);
+  }
 }
